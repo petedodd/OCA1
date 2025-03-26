61ed54de3a2f316721830a5129336d07b96cff6e
markdunning <mark.dunning@gmail.com>
2025-03-26 20:55:39 +0000

    Tidyup plotting imports

--- R/outputhandling.R
+++ R/outputhandling.R
@@ -62,7 +62,7 @@ getRateFromOut <- function(outdata){
 ##' out                               #inspect
 ##' ## visualize
 ##' plt_DemoGrowth(out)  
-##' @import ggplot2
+##' @importFrom ggplot2 ggplot geom_point geom_line ylab ggtitle scale_y_continuous theme_bw expand_limits
 ##' @import data.table
 ##' @export
 plt_DemoGrowth <- function(outdata) {
@@ -72,17 +72,17 @@ plt_DemoGrowth <- function(outdata) {
   cntry <- "GBR"
   N <- OCA1::UKdemo$N
   tc <- seq(from = round(min(outdat$t)), to = round(max(outdat$t)), by = 1)
-  ggplot2::ggplot(
+  ggplot(
     N[iso3 == cntry & Year %in% tc, .(pop = sum(PopTotal)), by = Year],
     aes(Year, pop)
   ) +
-    ggplot2::geom_point() +
-    ggplot2::geom_line(data = outdat[!grepl("rate", state), .(pop = sum(value)), by = t], aes(t, pop), col = 2) +
-    ggplot2::ylab("Population (in thousands)") +
-    ggplot2::ggtitle(cntry) +
-    ggplot2::scale_y_continuous(labels = absspace) +
-    ggplot2::theme_bw()+
-    ggplot2::expand_limits(y=c(0,NA))
+    geom_point() +
+    geom_line(data = outdat[!grepl("rate", state), .(pop = sum(value)), by = t], aes(t, pop), col = 2) +
+    ylab("Population (in thousands)") +
+    ggtitle(cntry) +
+    scale_y_continuous(labels = absspace) +
+    theme_bw()+
+    expand_limits(y=c(0,NA))
 }
 
 checkDemoFit <- function(outdata){
@@ -125,10 +125,11 @@ checkDemoFit <- function(outdata){
 ##' out                               #inspect
 ##' ## visualize
 ##' plt_DemoSnapshots(out)  
-##' @import ggplot2
-##' @import ggpubr
+##' @importFrom ggplot2 ggplot geom_point geom_line coord_flip ylab xlab scale_y_continuous theme_bw theme facet_wrap aes
 ##' @import data.table
-##' @import dplyr
+##' @importFrom dplyr mutate group_by slice ungroup rename filter select left_join
+##' @importFrom stringr str_replace_all
+##' @importFrom tidyr pivot_longer
 ##' @export
 plt_DemoSnapshots <- function(outdata) {
   
@@ -138,10 +139,10 @@ plt_DemoSnapshots <- function(outdata) {
   
   ## Get expected populations for males and females
   N <- OCA1::UKdemo$N |>
-    dplyr::select(t = Year, AgeGrp,PopMale,PopFemale) |>
-    tidyr::pivot_longer(contains("Pop"), values_to = "expected",names_to = "sex") |> 
-    dplyr::mutate(sex = stringr::str_replace_all(sex,"PopMale","M")) |>
-    dplyr::mutate(sex = stringr::str_replace_all(sex,"PopFemale","F"))
+    select(t = Year, AgeGrp,PopMale,PopFemale) |>
+    pivot_longer(contains("Pop"), values_to = "expected",names_to = "sex") |> 
+    mutate(sex = str_replace_all(sex,"PopMale","M")) |>
+    mutate(sex = str_replace_all(sex,"PopFemale","F"))
    
   ## calculate years at intervals of 5  
   tmz <- seq(from = round(min(outdat$t)), to = round(max(outdat$t)), by = 5)
@@ -149,30 +150,31 @@ plt_DemoSnapshots <- function(outdata) {
   
     ## get the calculated populations  
     tmpo <- outdat[!grepl("rate", state), .(value = sum(value)), by = .(t, sex, AgeGrp)] |>
-    dplyr::mutate(t = round(t)) |>
-    dplyr::group_by(t, sex, AgeGrp) |>
-    dplyr::slice(1) |>
-    dplyr::ungroup() |>
-    dplyr::rename("calculated" = value) |>
-    dplyr::mutate(AgeGrp = as.ordered(AgeGrp))
+    mutate(t = round(t)) |>
+    group_by(t, sex, AgeGrp) |>
+    slice(1) |>
+    ungroup() |>
+    rename("calculated" = value) |>
+    mutate(AgeGrp = as.ordered(AgeGrp))
   
   
     ## make joined data frame for plotting
-    combined <- dplyr::left_join(tmpo,N, by = c("t","sex","AgeGrp")) |>
-      dplyr::mutate(calculated = ifelse(sex == "M", -calculated,calculated)) |> 
-      dplyr::mutate(expected = ifelse(sex == "M", -expected, expected)) |>
-      dplyr::filter(t %in% tmz)
     
-    ggplot2::ggplot(combined, aes(x = AgeGrp, col=sex, shape=sex)) + 
-      ggplot2::geom_point(aes(y = expected,group=sex)) + 
-      ggplot2::geom_line(aes(y = calculated, group = sex)) +
-      ggplot2::coord_flip() +
-      ggplot2::ylab("Population (in thousands)") +
-      ggplot2::xlab("Age group") +
-      ggplot2::scale_y_continuous(labels = absspace) +
-      ggplot2::theme_bw() +
-      ggplot2::theme(legend.position = "none") + 
-      ggplot2::facet_wrap(~t,ncol = nr)
+    combined <- left_join(tmpo,N, by = c("t","sex","AgeGrp")) |>
+      mutate(calculated = ifelse(sex == "M", -calculated,calculated)) |> 
+      mutate(expected = ifelse(sex == "M", -expected, expected)) |>
+      filter(t %in% tmz)
+    
+    ggplot(combined, aes(x = AgeGrp, col=sex, shape=sex)) + 
+      geom_point(aes(y = expected,group=sex)) + 
+      geom_line(aes(y = calculated, group = sex)) +
+      coord_flip() +
+      ylab("Population (in thousands)") +
+      xlab("Age group") +
+      scale_y_continuous(labels = absspace) +
+      theme_bw() +
+      theme(legend.position = "none") + 
+      facet_wrap(~t,ncol = nr)
       
   
 
@@ -197,8 +199,7 @@ plt_DemoSnapshots <- function(outdata) {
 ##' out                               #inspect
 ##' ## visualize
 ##' plt_TBSnapshots(out, "natcat") 
-##' @import ggplot2
-##' @import ggpubr
+##' @importFrom ggplot2 ggplot coord_flip geom_bar scale_y_continuous scale_fill_manual facet_wrap theme_bw theme geom_hline ggtitle ylab xlab aes element_text element_blank
 ##' @import data.table
 ##' @export
 
@@ -244,20 +245,20 @@ plt_TBSnapshots <- function(outdata, by_layer = "natcat") {
   tmp[, state := factor(state, levels = c("Learly", "Llate", "Asymp", "Symp", "Treat"))]
   
   # Generate the population pyramid plot
-  pl <- ggplot2::ggplot(tmp, aes(x = AgeGrp, fill = state)) +
-    ggplot2::coord_flip() +
-    ggplot2::geom_bar(data = tmp[sex == 'M'], stat = 'identity', aes(y = value)) +
-    ggplot2::geom_bar(data = tmp[sex == 'F'], stat = 'identity', aes(y = -value)) +
-    ggplot2::scale_y_continuous(labels = function(x) format(abs(x), big.mark = ",", scientific = FALSE, trim = TRUE)) +
-    ggplot2::scale_fill_manual(values = mycols) +
-    ggplot2::facet_wrap(as.formula(paste0("`", new_layer_name, "` ~ t")), scales = 'free', nrow = 2) +
-    ggplot2::theme_bw() +
-    ggplot2::theme(axis.text.x = element_text(angle = 90, hjust = 1),
+  pl <- ggplot(tmp, aes(x = AgeGrp, fill = state)) +
+    coord_flip() +
+    geom_bar(data = tmp[sex == 'M'], stat = 'identity', aes(y = value)) +
+    geom_bar(data = tmp[sex == 'F'], stat = 'identity', aes(y = -value)) +
+    scale_y_continuous(labels = function(x) format(abs(x), big.mark = ",", scientific = FALSE, trim = TRUE)) +
+    scale_fill_manual(values = mycols) +
+    facet_wrap(as.formula(paste0("`", new_layer_name, "` ~ t")), scales = 'free', nrow = 2) +
+    theme_bw() +
+    theme(axis.text.x = element_text(angle = 90, hjust = 1),
                    legend.position = "bottom", legend.title = element_blank()) +
-    ggplot2::geom_hline(yintercept = 0, col = 'grey') +
-    ggplot2::ggtitle(paste("Population distribution by Age, Sex,", dyn_name, "and TB status")) +
-    ggplot2::ylab("Population") +
-    ggplot2::xlab("")
+    geom_hline(yintercept = 0, col = 'grey') +
+    ggtitle(paste("Population distribution by Age, Sex,", dyn_name, "and TB status")) +
+    ylab("Population") +
+    xlab("")
   
   pl
 }
@@ -284,8 +285,8 @@ plt_TBSnapshots <- function(outdata, by_layer = "natcat") {
 ##' out                               #inspect
 ##' ## visualize
 ##' plt_TBRates(out, rate_type = "incidence", by_layer = "natcat")
-##' @import ggplot2
-##' @import ggpubr
+##' @importFrom ggplot2 ggplot facet_grid geom_line ylab ggtitle theme_light xlab theme guides aes element_blank guide_legend
+##' @importFrom dplyr mutate group_by summarise recode
 ##' @import data.table
 ##' @export
 
@@ -313,15 +314,15 @@ plt_TBRates <- function(outdata, rate_type = "incidence", by_layer = "natcat") {
   
   # select the target rate
   tmp <- outdat[state == state_map[[rate_type]]] |>
-    dplyr::mutate(state = dplyr::recode(state,
+    mutate(state = recode(state,
                                         rate_Incidence = "Incidence rate",
                                         rate_Notification = "Notification rate",
                                         rate_TBmortality = "TB mortality rate")) |>
     
-    dplyr::group_by(t, state, AgeGrp, sex, .data[[by_layer]]) |>
+    group_by(t, state, AgeGrp, sex, .data[[by_layer]]) |>
     
     # Compute the mean value for each group, ignoring NA values
-    dplyr::summarise(value = mean(value, na.rm = TRUE), .groups = "drop") |>
+    summarise(value = mean(value, na.rm = TRUE), .groups = "drop") |>
     as.data.table()
   
   # Retrieve the maped layer namer for the selected layer
@@ -336,20 +337,20 @@ plt_TBRates <- function(outdata, rate_type = "incidence", by_layer = "natcat") {
   # plot title based on the first available state name;
   plot_title <- if (nrow(tmp) > 0) tmp$state[1] else "No Data"
   
-  ggplot2::ggplot(tmp, ggplot2::aes(t, value, col = AgeGrp)) +
+ggplot(tmp, aes(t, value, col = AgeGrp)) +
     # Create facet grid for stratification layer and sex (columns)
-    ggplot2::facet_grid(as.formula(paste0("`", new_layer_name, "` ~ sex"))) + 
-    ggplot2::geom_line() + 
-    ggplot2::ylab("Rate per 100,000 population") +
-    ggplot2::ggtitle(plot_title) +
-    ggplot2::theme_light() + 
-    ggplot2::xlab("") +
+    facet_grid(as.formula(paste0("`", new_layer_name, "` ~ sex"))) + 
+    geom_line() + 
+    ylab("Rate per 100,000 population") +
+    ggtitle(plot_title) +
+    theme_light() + 
+    xlab("") +
     
     # Adjust legend position and remove title
-    ggplot2::theme(legend.position = "bottom", legend.title = element_blank()) +
+    theme(legend.position = "bottom", legend.title = element_blank()) +
     
     # Adjust legend to have 7 columns for better readability
-    ggplot2::guides(col = guide_legend(ncol = 7))
+    guides(col = guide_legend(ncol = 7))
 }
 
 # plt_TBRates(out, rate_type = "incidence", by_layer = "natcat")
